baa97da311c808148f909c79261b59df83a7a7a5
Aman <foraman202514@gmail.com>
2026-09-19 21:57:52 +0530

    Added NumPy Array Creation

---
+++ _01NumPyBasics/_01NumPyArrayCreation.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# Array creation in numpy
+a = np.array([1,2,3])
+print(a)
+print(a.dtype)
+
+f = np.array([1.2,1.3,1.4])
+print(f)
+print(f.dtype)
+
+# Error in Numpy
+# a = np.array(1,2,3) # TypeError: array() takes from 1 to 2 positional arguments but 3 were given
